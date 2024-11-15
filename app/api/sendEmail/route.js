21c35703119ec49bb9b69285ef0009fd9a27bb70
DavidSantosTXUSA <dsantos4148@gmail.com>
2024-11-14 19:13:32 -0600

    REMOVED CONSOLE LOGS. FIXED EMAIL ERROR ON DEPLOYMENT. DEPLOYMENT WORKS EXACTLY LIKE LOCALHOST!

--- app/api/sendEmail/route.js
+++ app/api/sendEmail/route.js
@@ -1,20 +1,15 @@
 import sendEmail from './sendEmail';
 
 export async function POST(req) {
-  console.log("POST request received at /api/sendEmail"); // Add this line
   try {
     const formData = await req.json();
-    console.log("Received form data:", formData); // Log form data
     const result = await sendEmail(formData);
     if (result.success) {
-      console.log("Email sent successfully."); // Log success
       return new Response(JSON.stringify({ success: true }), { status: 200 });
     } else {
-      console.error("Failed to send email:", result.error); // Log specific error message
       return new Response(JSON.stringify({ success: false, error: result.error }), { status: 500 });
     }
   } catch (error) {
-    console.error("Error in POST request:", error); // Log detailed error for POST
     return new Response(JSON.stringify({ success: false, error: error.message }), { status: 500 });
   }
 }
--- app/api/sendEmail/sendEmail.js
+++ app/api/sendEmail/sendEmail.js
@@ -1,16 +1,10 @@
 import nodemailer from 'nodemailer';
-console.log("sendEmail function is being called");
-export default async function sendEmail({ name, email, message }) {
-  console.log("Environment Variables:");
-  console.log("SMTP_USER:", process.env.SMTP_USER);
-  console.log("SMTP_PASSWORD:", process.env.SMTP_PASSWORD);
-  console.log("SMTP_HOST:", process.env.SMTP_HOST);
-  console.log("SMTP_PORT:", process.env.SMTP_PORT);
 
+export default async function sendEmail({ name, email, message }) {
   const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: Number(process.env.SMTP_PORT),
-    secure: false, 
+    secure: false,
     auth: {
       user: process.env.SMTP_USER,
       pass: process.env.SMTP_PASSWORD,
@@ -24,10 +18,8 @@ export default async function sendEmail({ name, email, message }) {
       subject: `New message from ${name}`,
       text: `Email: ${email}\n\nMessage:\n${message}`,
     });
-    console.log("Email sent:", info);
     return { success: true };
   } catch (error) {
-    console.error("Error sending email:", error);
     return { success: false, error: error.message };
   }
 }
