114c62afec36346c806fa5edf68afd75a4a25caa
DavidSantosTXUSA <dsantos4148@gmail.com>
2024-11-14 19:16:58 -0600

    added back console log for info to bypass error on build.

--- app/api/sendEmail/sendEmail.js
+++ app/api/sendEmail/sendEmail.js
@@ -1,16 +1,15 @@
 import nodemailer from 'nodemailer';
-
 export default async function sendEmail({ name, email, message }) {
+
   const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: Number(process.env.SMTP_PORT),
-    secure: false,
+    secure: false, 
     auth: {
       user: process.env.SMTP_USER,
       pass: process.env.SMTP_PASSWORD,
     },
   });
-
   try {
     const info = await transporter.sendMail({
       from: process.env.SMTP_USER,
@@ -18,8 +17,10 @@ export default async function sendEmail({ name, email, message }) {
       subject: `New message from ${name}`,
       text: `Email: ${email}\n\nMessage:\n${message}`,
     });
+    console.log("Email sent:", info);
     return { success: true };
   } catch (error) {
+    console.error("Error sending email:", error);
     return { success: false, error: error.message };
   }
 }
