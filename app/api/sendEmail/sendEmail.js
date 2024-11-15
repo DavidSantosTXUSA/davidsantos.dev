fae70ac312a6b6f4f4a3151d64b06dfc4ac7c8a3
DavidSantosTXUSA <dsantos4148@gmail.com>
2024-11-14 18:42:10 -0600

    attempting to fix email bugs in deployment

--- app/api/sendEmail/sendEmail.js
+++ app/api/sendEmail/sendEmail.js
@@ -9,7 +9,7 @@ export default async function sendEmail({ name, email, message }) {
 
   const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
-    port: process.env.SMTP_PORT,
+    port: Number(process.env.SMTP_PORT),
     secure: false, 
     auth: {
       user: process.env.SMTP_USER,
