bb424ffafc8ac66779d0804f230a3b2cb3c115a9
DavidSantosTXUSA <dsantos4148@gmail.com>
2024-11-19 16:44:56 -0600

    fixed deployment error with Image being defined but not used

--- app/training/page.tsx
+++ app/training/page.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import Image from 'next/image';
+/*import Image from 'next/image';
 /* eslint-disable react/no-unescaped-entities */  
 
 export default function Training() {
